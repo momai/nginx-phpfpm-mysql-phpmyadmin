13bd56f64800d729281fc53df1495e9d05e34a33
momai <mamai2010@gmail.com>
2022-10-22 14:39:32 +0000

    fix backupbd

--- backupbd.sh
+++ backupbd.sh
@@ -3,11 +3,19 @@
 #env file
 . ~/nginx-phpfpm-mysql-phpmyadmin/.env
 
+
+
 #date
 DATA=`date +"%Y-%m-%d_%H-%M"`
 
 #backup dir
 backupdir="$HOMEDIR/backupbd"
+
+#if [[ ! -e "$backupdir" ]]; then
+    mkdir -p $backupdir
+#elif [[ ! -d "$backupdir" ]]; then
+#    echo "$backupdir already exists but is not a directory" 1>&2
+#fi
 
 docker exec mysql /usr/bin/mysqldump --defaults-extra-file=/var/lib/mysql-credentials.cnf test > "$backupdir"/"$DATA"-test.sql
 /usr/bin/find "$backupdir" -type f -mtime +10 -exec rm -rf {} \;
